c11b8ccb563e9cd1ff429487e5ba0277cf0ef1eb
steven_savarin <srssdesing@gmail.com>
2024-11-25 19:09:38 +0100

    about section

---
+++ src/components/About.jsx
@@ -0,0 +1,63 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+import AnimatedTitle from "./AnimatedTitle";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Efraín Silva
+        </p>
+
+        <AnimatedTitle
+          title="Padr<r>e</r> de la guitarra<br /> clasica del estado <b>a</b>ragua"
+          containerClass="mt-5 !text-black text-center"
+        />
+
+        <div className="about-subtext">
+          <p>Uno de los máximos exponentes de la tradición guitarrista venezolana</p>
+          <p className="text-gray-500">
+          Estudió y se graduó en el Conservatorio de Santa Cecilia en Roma, 
+          donde también recibió una maestría en física acústica.
+          </p>
+        </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
