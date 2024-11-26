9ddd66ffdba04ea5068f9aabf9a73f828301c859
steven_savarin <srssdesing@gmail.com>
2024-11-26 21:33:34 +0100

    features

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
 import NavBar from "./components/Navbar";
+import Features from './components/Features.jsx'
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <NavBar />
       <Hero />
       <About />
+      <Features />
     </main>
   ) 
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -75,7 +75,7 @@ const NavBar = () => {
 
             <Button
               id="product-button"
-              title="Products"
+              title="Contacto"
               rightIcon={<TiLocationArrow />}
               containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
             />
